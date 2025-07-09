58bc6cb75f0e77565e8f5f7ae5abee56cf66e97c
Cristina <kamuthucristina@gmail.com>
2025-07-09 17:22:39 +0300

    fied the stylings of the tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -6,9 +6,10 @@ export default function tabsLayout() {
   return <Tabs screenOptions={{
     tabBarActiveTintColor:"black",
   }} >
-    <Tabs.Screen name="index" options={{headerTitle:"Hello World", tabBarIcon: ({focused, color}) => <Ionicons name={focused ? "information-circle": "information-circle-outline"} color={color} size={30}/> }}
+    <Tabs.Screen name="index" options={{headerTitle:"Hello World", tabBarIcon: ({focused, color}) => <Ionicons name={focused ? "home-sharp": "home-outline"} color={color} size={30}/> }}
     />
-    <Tabs.Screen name="about" options={{headerTitle:"about"}}/>
+    <Tabs.Screen name="about" options={{headerTitle:"about", tabBarIcon: ({focused, color}) => <Ionicons  name={focused ? "information-circle": "information-circle-outline"} color={color} size={30}/>
+}}/>
     <Tabs.Screen name="+not-found" options={{headerShown:false,}} />
   </Tabs>;
 }
