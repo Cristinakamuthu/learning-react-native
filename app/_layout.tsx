0251bb26c29b364abc07edb0f86cfc8697d66f88
Cristina <kamuthucristina@gmail.com>
2025-07-09 17:18:34 +0300

    added the tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,17 @@
 import { HeaderTitle } from "@react-navigation/elements";
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { LogBox } from "react-native";
+
+LogBox.ignoreAllLogs(true);
 
 export default function RootLayout() {
-  return <Stack>
-    <Stack.Screen name="index" options={{headerTitle:"Hello World,", headerLeft: () => <></>,}}/>
-    <Stack.Screen name="about" options={{headerTitle:"about"}}/>
+  return <>
+  <StatusBar style="light" />
+  <Stack>
+    <Stack.Screen name="(tabs)" options={{headerShown: false,}}/>
+    
     <Stack.Screen name="+not-found" options={{headerShown:false,}} />
   </Stack>;
+  </>
 }
