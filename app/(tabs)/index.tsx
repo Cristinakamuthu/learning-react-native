4621d1c3d8b9c42490f80c5026a6b4e6da324e5d
Cristina <kamuthucristina@gmail.com>
2025-07-09 21:37:32 +0300

    added image

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,16 @@
 import { Link } from "expo-router";
 import {StyleSheet, Text, View } from "react-native";
+import { Image } from "expo-image";
 
+const PlaceholderImage = require("../../assets/images/home.avif")
 export default function Index() {
   return (
     <View style={styles.container}>
+      <View style={styles.imagecontainer}>
+      <Image source={PlaceholderImage}style={styles.image} />
+      </View>
       <Text>Hello world!</Text>
-      <Link href={"/about"} style={styles.button}> Go to about screen </Link>
+      {/* <Link href={"/about"} style={styles.button}> Go to about screen </Link> */}
     </View>
   );
 }
@@ -14,17 +19,25 @@ export default function Index() {
 const styles = StyleSheet.create({
   container:{
     flex:1,
-    backgroundColor:'#fff',
+    // backgroundColor:'#fff',
     alignItems:'center',
     justifyContent:'center',
   
   },
-  button:{
-    fontSize:20,
-    textDecorationLine:"underline",
-    color:"black",
+  // button:{
+  //   fontSize:20,
+  //   textDecorationLine:"underline",
+  //   color:"black",
 
+  // },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
   },
+  imagecontainer:{
+    flex: 1,
+  }
   
 });
 
